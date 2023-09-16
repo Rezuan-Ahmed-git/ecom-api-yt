4caaa4c49b4710992a4cf46caeb45b53528207b0
rezuan <rezuan.ahmed.pro@gmail.com>
2023-09-16 07:24:28 +0600

    feat: error handler implement

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const express = require('express');
 const dbConnect = require('./config/dbConnect');
 const authRouter = require('./routes/authRoute');
 const bodyParser = require('body-parser');
+const { notFound, errorHandler } = require('./middlewares/errorHandler');
 
 dbConnect();
 const app = express();
@@ -13,6 +14,10 @@ app.use(bodyParser.urlencoded({ extended: false }));
 const PORT = process.env.PORT || 4000;
 
 app.use('/api/user', authRouter);
+
+//error
+app.use(notFound);
+app.use(errorHandler);
 
 app.listen(PORT, () => {
   console.log(`Server is running at PORT ${PORT}`);
